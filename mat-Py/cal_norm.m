6a587a09145939e7aeecff1ff6bd2e99214b9011
Mint <lyg_hjp@163.com>
2018-09-08 14:00:22 +0800

    Delete cal_norm.m

--- mat-Py/cal_norm.m
+++
@@ -1,8 +0,0 @@
-function result = cal_norm(origin, reconstruction)
-
-result = 0;
-error = origin -reconstruction;
-[dim1, dim2, dim3] = size(origin);
-for i = 1:dim3
-    result = result + norm(error(:, :, i));
-end
