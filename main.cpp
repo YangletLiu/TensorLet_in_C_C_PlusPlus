5db90aa12026ca5d5fc73fadd693a3ebe40dc70a
HF-Mint <lyg_hjp@163.com>
2019-01-10 10:48:59 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -18,39 +18,39 @@ using namespace std;
 
 int main(){
     MKL_INT n1,n2,n3;
+    n1=n2=n3=100;
+
     MKL_INT aa[3]={10,10,10};
 
     double t0,t1;
 
     t0=gettime();
-    Tensor3D<double> a(100,100,100); //element
+    Tensor3D<double> a(n1,n2,n3); //element
     t1=gettime();
-    cout << "time:" <<t1-t0 <<endl;
+    cout << "Initialize time:" <<t1-t0 <<endl;
 
-    cout << sizeof(a) << endl;
-    cout << a.getsize()[2] << endl;
+    cout << "size of class: " << sizeof(a) << endl;
+
+    int *p1;
+    p1 = a.getsize();
+    cout << "shape: " << p1[2] << endl;
     cout << "element a: " << a(2,2,2) << endl;
     cout << "norm: " << a.frobenius_norm() << endl;
 
     Tensor3D<double> b(aa); // int array
     b = a;
     b += a;
-    cout << b.getsize()[0] << endl;
+    cout << "shape: " << b.getsize()[2] << '\n';
 
-    cout << "n" << b.getsize()[2];
-
-    double *cc = (double*)mkl_calloc(10000,sizeof(double),64);  //返回成功为1
-    cout << sizeof(cc) << endl;
-    cout << cc << endl;
-    mkl_free(cc);
 
     Tensor3D<double> vd(a);
-    cout << vd.getsize()[1] << endl;
+    cout << "shape: " << vd.getsize()[1] << endl;
 
     MKL_INT k = 2;
     b = k*a;
     b = a*b;
 //    b = a+b+a;
+
 
     t0=gettime();
     double * p = (double*)mkl_calloc(1000000,sizeof(double),64);
@@ -59,7 +59,7 @@ int main(){
     cout << "time:" <<t1-t0 <<endl;
     mkl_free(p);
 
-    //随机数生成
+//随机数生成
     t0=gettime();
     double r[1000000];
     VSLStreamStatePtr stream;
@@ -89,6 +89,12 @@ int main(){
 //    printf("\n");
 
     cout << "hello" << endl;
+
+// alloc memory
+//    double *cc = (double*)mkl_calloc(10000,sizeof(double),64);  //返回成功为1
+//    cout << sizeof(cc) << endl;
+//    cout << cc << endl;
+//    mkl_free(cc);
 
     return 0;
 }
